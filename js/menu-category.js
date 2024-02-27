362faa1f5d0fa7c7f9517fd580876a39b034a092
Gusoft <161398726+Gusoftproyects@users.noreply.github.com>
2024-02-26 19:18:17 -0600

    Add files via upload

---
+++ js/menu-category.js
@@ -0,0 +1,12 @@
+let btnMostrar = document.getElementById("btnMostrar");
+
+function mostrarMenu() {
+    var divMenu = document.getElementById("main-category");
+    if (divMenu.style.display === "none") {
+        divMenu.style.display = "block";
+    } else{
+        divMenu.style.display = "none";
+    }
+}
+
+// carrusel
